6a5e37f5c665ae67bdca87fc10d06c42b3919158
zeynep betül çelik <zeynepbetulcelk@gmail.com>
2023-06-06 17:55:33 +0300

    welcome gif added

--- src/welcome.js
+++ src/welcome.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect } from 'react';
 import { BackHandler, Dimensions, Image, ImageBackground, StatusBar, StyleSheet, Text, View, } from 'react-native';
 import { darkPurple, whiteColor } from './theme';
 import { Button, } from 'react-native-elements';
-import AnimatedLottieView from 'lottie-react-native';
+import LottieView from 'lottie-react-native';
 import { useNavigation } from "@react-navigation/native";
 
 
@@ -17,16 +17,16 @@ export default welcome = (props) => {
     return (
         <View style={styles.container}>
             <StatusBar hidden={true} showHideTransition={true} barStyle="dark-content" />
-
             <ImageBackground source={require("../assets/images/welcome-bg.png")} resizeMode="cover" style={styles.image}>
                 <View style={{ flex: 1, justifyContent: "space-around" }}>
                     <Image source={require("../assets/askStudentSplash.png")} style={styles.logo} />
-                    <AnimatedLottieView source={require('../assets/images/confetti.json')} autoPlay loop />
                     <View style={{ alignItems: "center" }}>
-                        <Text style={{ fontSize: 18, color: whiteColor, }}>Her şey hazır!</Text>
-                        <Text style={{ fontSize: 18, color: whiteColor }}>QueX’le soru çözmeye başla!</Text>
+                      <Image style={{ width: 300, height: 300 }} source={require('../assets/images/confetti.gif')} />
+                          <View style={{ alignItems: "center" }}>
+                             <Text style={{ fontSize: 18, color: whiteColor, }}>Her şey hazır!</Text>
+                             <Text style={{ fontSize: 18, color: whiteColor }}>QueX’le soru çözmeye başla!</Text>
+                         </View>
                     </View>
-
                     <Button title="Başla" onPress={() => navigation.navigate("Tab")} titleStyle={{ color: darkPurple, fontFamily: "Nunito-Bold" }} buttonStyle={{ backgroundColor: whiteColor }} containerStyle={{ backgroundColor: whiteColor, width: width * .6, paddingVertical: 5, alignSelf: "center", }} />
                 </View>
             </ImageBackground>
