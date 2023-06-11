b1c64ed27a1f0796c6f334f3aa497e61c52ade45
ht1625 <61890580+ht1625@users.noreply.github.com>
2023-06-12 01:08:46 +0300

    finish student

--- src/cardSlider.js
+++ src/cardSlider.js
@@ -85,7 +85,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     borderWidth: 1,
     borderColor: 'transparent',
-    width: 350,
+    width: width - 40,
   },
   iconContainer: {
     position: 'absolute',
